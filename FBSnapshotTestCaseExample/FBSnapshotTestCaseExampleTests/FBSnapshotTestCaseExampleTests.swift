7ccd8ff78a00f6dcf4b8fe797f03e102db7c3e5b
Federico Jordán <federicojordan@MacBook-Pro-de-Federico-Jordan.local>
2018-02-13 17:43:18 -0300

    finished first snapshot test

--- FBSnapshotTestCaseExample/FBSnapshotTestCaseExampleTests/FBSnapshotTestCaseExampleTests.swift
+++ FBSnapshotTestCaseExample/FBSnapshotTestCaseExampleTests/FBSnapshotTestCaseExampleTests.swift
@@ -6,31 +6,20 @@
 //  Copyright © 2018 Federico Jordán. All rights reserved.
 //
 
-import XCTest
+import FBSnapshotTestCase
 @testable import FBSnapshotTestCaseExample
 
-class FBSnapshotTestCaseExampleTests: XCTestCase {
+class FBSnapshotTestCaseExampleTests: FBSnapshotTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+        self.recordMode = false
     }
     
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let viewController: UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController")
+        FBSnapshotVerifyView(viewController.view)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
     
 }
---
+++ FBSnapshotTestCaseExample/Pods/Target Support Files/Pods-FBSnapshotTestCaseExampleTests/Pods-FBSnapshotTestCaseExampleTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FBSnapshotTestCaseExampleTests : NSObject
+@end
+@implementation PodsDummy_Pods_FBSnapshotTestCaseExampleTests
+@end
